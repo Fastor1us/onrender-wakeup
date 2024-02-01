6d4e11c420bb92d6343b8bf8bd9355e4f92358ef
Oleg Kiryushin <fewgwer3@yandex.ru>
2024-02-01 18:37:20 +0300

    init

--- README.md
+++ README.md
@@ -1,19 +1,3 @@
 # server-wakeup
 
-## Project setup
-```
-npm install
-```
-
-### Compiles and hot-reloads for development
-```
-npm run serve
-```
-
-### Compiles and minifies for production
-```
-npm run build
-```
-
-### Customize configuration
-See [Configuration Reference](https://cli.vuejs.org/config/).
+## Веб-сервис для поддержания работы серверов онлайн - автоматического подъёма по таймеру
--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,149 @@
-<template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
-</template>
+<script setup>
+import { computed, onMounted, ref } from 'vue';
 
-<script>
-import HelloWorld from './components/HelloWorld.vue'
+const SERVERS = [
+  {
+    name: 'game2cube',
+    endpoint: 'https://game2cube-backend.onrender.com/user/avatars',
+  },
+  {
+    name: 'portfolio',
+    endpoint: 'https://portfolio-okir-backend.onrender.com/projects',
+  },
+];
+const TIMER_DELAY = 870000; // 14.5 min
 
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
+let intervalFetch = null;
+let intervalTimer = null;
+const requestCount = ref(0);
+const endpointStats = ref({});
+const secondsPassed = ref(0);
+const fetchStatus = ref('');
+
+const resetEndpointStats = () => {
+  for (const server of SERVERS) {
+    endpointStats.value[server.endpoint] = 0;
   }
-}
+};
+resetEndpointStats();
+
+const resetTimerInterval = () => {
+  clearInterval(intervalTimer);
+  intervalTimer = null;
+  secondsPassed.value = 0;
+};
+
+const serversFetch = async () => {
+  fetchStatus.value = 'fetching...';
+  for (const server of SERVERS) {
+    try {
+      const response = await fetch(server.endpoint);
+      if (response.ok) {
+        endpointStats.value[server.endpoint]++;
+      }
+    } catch (error) {
+      console.error(error);
+    }
+  }
+  requestCount.value++;
+  fetchStatus.value = '';
+};
+
+const startIntervals = () => {
+  if (intervalFetch) return;
+  resetEndpointStats();
+  if (intervalTimer) resetTimerInterval();
+  intervalTimer = setInterval(() => {
+    secondsPassed.value++;
+  }, 1000);
+  console.log(1);
+  serversFetch();
+  console.log(4);
+  intervalFetch = setInterval(async () => {
+    serversFetch();
+  }, TIMER_DELAY);
+};
+
+const stopIntervals = () => {
+  clearInterval(intervalFetch);
+  intervalFetch = null;
+  resetTimerInterval();
+};
+
+const toggleInterval = () => {
+  if (intervalFetch) {
+    stopIntervals();
+  } else {
+    startIntervals();
+  }
+};
+
+const timeRamaining = computed(() => {
+  const secondsLeft = (TIMER_DELAY - secondsPassed.value * 1000) / 1000;
+  const minutes = Math.floor(secondsLeft / 60);
+  const seconds = secondsLeft % 60;
+  return `${minutes}:${seconds < 10 ? '0' : ''}${seconds}`;
+});
 </script>
 
+<template>
+  <section>
+    <div class="control-panel">
+      <button class="control-button" v-on:click="toggleInterval">
+        {{ intervalFetch ? 'Stop' : 'Start' }} service
+      </button>
+      <div class="control-indicator" :class="{ active: intervalFetch }"></div>
+      <div class="control-timer">Time remaining: {{ timeRamaining }}</div>
+    </div>
+    <div class="statistics">
+      <div style="display: flex; gap: 10px">
+        <p>Request Count: {{ requestCount }}</p>
+        <p>{{ fetchStatus }}</p>
+      </div>
+      <ul>
+        <li v-for="server in SERVERS" :key="server.endpoint">
+          {{ server.name }}: {{ endpointStats[server.endpoint] }}
+        </li>
+      </ul>
+    </div>
+  </section>
+</template>
+
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+body {
+  background-color: rgb(114, 129, 124);
+  color: white;
+  font-size: 20px;
+}
+
+.control-panel {
+  display: flex;
+  align-items: center;
+}
+
+.control-button {
+  width: 200px;
+  height: 30px;
+  font-size: 20px;
+}
+
+.control-indicator {
+  width: 30px;
+  height: 30px;
+  box-sizing: border-box;
+  background-color: red;
+  border: 2px solid black;
+}
+
+.control-indicator.active {
+  background-color: green;
+}
+
+.control-timer {
+  margin-left: 10px;
+}
+
+.statistics {
+  margin-top: 20px;
 }
 </style>
